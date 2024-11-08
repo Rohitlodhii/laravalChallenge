fbebfbcdb6aed06ba4bf133c186de714a9b509fd
rohitlodhii <rohitlodhi0701@gmail.com>
2024-11-08 17:26:37 +0530

    final typo fixes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Rohit Code Lab</title>
+    <title>Rohit Portfolio</title>
 
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
    
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Rohit's Lab</title>
+    <title>Rohit's Portfolio</title>
     <script src="https://kit.fontawesome.com/0730c4a9bc.js" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
